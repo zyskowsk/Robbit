170d2c35eb82de09dcf6e7d29efcf56007bb27c5
Rob Zyskowski <zyskowski.rob@gmail.com>
2013-08-15 10:52:47 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Robbit::Application.config.secret_token = '0e8bb367d8eb2654a896044ea6c56966311a351ec2f3b285b305d66525b2e0690a672aa4c3433d078423361b139476e3c9910963ffc3cd5b61cd592349e48333'
